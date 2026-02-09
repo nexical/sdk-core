55fed38810ad3beb1858033250cac9bb985f833d
Adrian Webb <adrian@webb.sh>
2026-02-09 10:52:52 -0500

    Removing the generated registry from the index source file.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 /* eslint-disable */
 import { ApiClient, type ApiClientOptions } from './core.js';
-import { initializeSdkRegistry, type SdkRegistry } from './registry.generated.js';
 
 export * from './core.js';
-
-export interface NexicalClient extends SdkRegistry { }
 
 export class NexicalClient extends ApiClient {
   constructor(options: ApiClientOptions) {
     super(options);
-    Object.assign(this, initializeSdkRegistry(this));
   }
 }
